4448a9b95e57f6bfbde57968273f004b0dee4236
@SanPanda <deivis9010@gmail.com>
2025-08-23 16:44:41 -0300

    feat: add Postman collection for employee management API

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const employeesController = require("../controllers/employees.controller.js");
 
 router.get('/', employeesController.getAllEmployees);
-router.post('/', employeesController.createEmployee);
 router.get('/oldest', employeesController.getOldestEmployee);
-router.get('/name/:name', employeesController.getEmployeeByName);
+router.post('/', employeesController.createEmployee);
+router.get('/:name', employeesController.getEmployeeByName);
 
 module.exports = router;
